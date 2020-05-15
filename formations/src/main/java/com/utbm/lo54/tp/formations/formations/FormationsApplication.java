5ce1fa8fda39f8292a4bc8e78681335867cefd56
komi agotsi <cyrilleagotsi@gmail.com>
2020-05-15 23:19:44 +0200

    Init project structure.

---
+++ formations/src/main/java/com/utbm/lo54/tp/formations/formations/FormationsApplication.java
@@ -0,0 +1,13 @@
+package com.utbm.lo54.tp.formations.formations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FormationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FormationsApplication.class, args);
+	}
+
+}
---
+++ formations/src/test/java/com/utbm/lo54/tp/formations/formations/FormationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.utbm.lo54.tp.formations.formations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FormationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
